cd436b00be871f8c4f8d19b617ef12bddc6b3b56
Gopal-Govind-I2I <84776629+Gopal-Govind-I2I@users.noreply.github.com>
2021-06-04 02:02:37 +0530

    Update EmployeeManagementLogger.java

--- src/main/java/com/ideas2it/logger/EmployeeManagementLogger.java
+++ src/main/java/com/ideas2it/logger/EmployeeManagementLogger.java
@@ -26,68 +26,68 @@ public class EmployeeManagementLogger {
     }
     
     /**
-	 * Logs debug object and message
-	 * 
-	 * @param message String type message
-	 * @param debug Object type debug
-	 */
-	public void logDebug(String message, Object debug) {
-		logger.debug(debug);
-	}
+     * Logs debug object and message
+     * 
+     * @param message String type message
+     * @param debug Object type debug
+     */
+     public void logDebug(String message, Object debug) {
+         logger.debug(debug);
+     }
 	
     /**
-	 * Logs info object and message
-	 * 
-	 * @param message String type message
-	 * @param info Object type info
-	 */
-	public void logInfo(String message, Object info) {
-		logger.info(info);
-	}
+     * Logs info object and message
+     * 
+     * @param message String type message
+     * @param info Object type info
+     */
+     public void logInfo(String message, Object info) {
+         logger.info(info);
+     }
 	
-	/**
-	 * Logs info message
-	 * @param message String type message
-	 */
-	public void logInfo(String message) {
-		logger.info(message);
-	}
+    /**
+     * Logs info message
+     * @param message String type message
+     */
+     public void logInfo(String message) {
+         logger.info(message);
+     }
 	
-	/**
-	 * Logs warning object and message
-	 * 
-	 * @param message String type message
-	 * @param warning Object type warning
-	 */
-	public void logWarn(String message, Object warning) {
-		logger.warn(warning);
-	}
+    /**
+     * Logs warning object and message
+     * 
+     * @param message String type message
+     * @param warning Object type warning
+     */
+     public void logWarn(String message, Object warning) {
+         logger.warn(warning);
+     }
 	
-	/**
-	 * Logs warning message
-	 * 
-	 * @param message String type message
-	 */
-	public void logWarn(String message) {
-		logger.warn(message);
-	}
+    /**
+     * Logs warning message
+     * 
+     * @param message String type message
+     */
+     public void logWarn(String message) {
+         logger.warn(message);
+     }
 	
-	/**
-	 * Logs error object
-	 * 
-	 * @param error Object type error
-	 */
-	public void logError(Throwable throwable) {
-		logger.error(throwable);
-	}
+    /**
+     * Logs error object
+     * 
+     * @param error Object type error
+     */
+     public void logError(Throwable throwable) {
+         logger.error(throwable);
+     }
 	
-	/**
-	 * Logs fatal object
-	 * 
-	 * @param message String type message
-	 * @param fatal Object type fatal
-	 */
-	public void logFatal(String message, Object fatal) {
-		logger.fatal(fatal);
-	}
+    /**
+     * Logs fatal object
+     * 
+     * @param message String type message
+     * @param fatal Object type fatal
+     */
+     public void logFatal(String message, Object fatal) {
+         logger.fatal(fatal);
+     }
 }
